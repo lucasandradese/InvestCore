e093f712038eff05d465be157f5a83c8af04ba13
lucasandradese <lucasandradese@outlook.com>
2026-01-21 21:20:54 -0300

    first commit

---
+++ src/test/java/dev/lucasandadese/InvestCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucasandadese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
